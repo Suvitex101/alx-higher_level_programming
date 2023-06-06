2479e68b14e0257505706d094abfa52857f3d83d
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-06-06 15:15:29 -0700

    python function

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """Function to computes power."""
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    """function that prints the numbers from 1 to 100 separated by a space."""
+    for i in range(0, 101):
+        if i % 3 == 0:
+            print("Fizz ", end=" ")
+        elif i % 5 == 0:
+            print("Buzz ", end=" ")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
+    print("$", end="")
