710e4d31e5b22ce424e239227f58085c75339459
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-06-26 14:33:31 -0700

    python Execptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+from __future__ import print_function
+
+def safe_function(fct, *args):
+    """function that executes a function safely."""
+    try:
+        tmp = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    else:
+        return tmp
