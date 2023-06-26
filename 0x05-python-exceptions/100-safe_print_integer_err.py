96e7c5eaa5c7b8d8091b019830fa8936f09e9747
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2023-06-26 14:39:53 -0700

    python Execptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
+
 def safe_print_integer_err(value):
     """function that prints an integer."""
+    import sys
     try:
         print("{:d}".format(value))
         return (True)
