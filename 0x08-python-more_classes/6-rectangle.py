b9b55d49450868b99fd8b0bc54ef4bf9fc7dad19
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2024-01-05 01:36:51 +0100

    python class

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,12 +6,15 @@
 class Rectangle:
     """a rectangle."""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
