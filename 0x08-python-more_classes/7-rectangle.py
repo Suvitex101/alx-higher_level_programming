bf566b9aafbb5de5770a9d871f58e983c61dfe9a
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2024-01-05 01:41:27 +0100

    python class

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,7 +4,10 @@
 
 
 class Rectangle:
-    """a rectangle."""
+    """a rectangle.
+    Attributes:
+    number_of_instances (int): The number of Rectangle instances.
+    print_symbol (any): The symbol used for string representation."""
 
     number_of_instances = 0
     print_symbol = "#"
