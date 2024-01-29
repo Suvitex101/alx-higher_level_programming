1c03ce6c39d1df92aa27f281937fbc27a01a1072
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2024-01-29 08:16:27 +0100

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Python inheritance"""
+
+
+class MyList(list):
+    """Class to print sorted list"""
+    
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
