dee360aedabe5499e106b7ecc2046da378b704c7
Suvitex101 <oluwaniyibalogun1998@gmail.com>
2024-01-29 08:53:41 +0100

    base geomentry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """A base geometry."""
+    
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
